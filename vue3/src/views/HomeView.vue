993b30b9feee7fd40ad0ebe9280f8912d72478ff
liuyong.wang.o <liuyonng.wang.o@nio.com>
2024-06-27 19:23:28 +0800

    封装一个鼠标点击位置获取hooks

--- vue3/src/views/HomeView.vue
+++ vue3/src/views/HomeView.vue
@@ -1,18 +1,12 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <div>x的坐标: {{ x }}</div>
+    <div>y的坐标: {{ y }}</div>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-
-export default defineComponent({
-  name: 'HomeView',
-  components: {
-    HelloWorld,
-  },
-});
+<script lang="ts" setup>
+import useMouse from "@/hooks/useMouse";
+const { x, y } = useMouse();
 </script>

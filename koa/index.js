1eea99b6d1a80e6389385b6946fc9b5ee3c62437
liu.yong.wang.o <liuyong.wang.o@nio.com>
2024-06-28 15:46:01 +0800

    koa   middleware

--- koa/index.js
+++ koa/index.js
@@ -1,12 +1,21 @@
-const Koa = require("koa");
+const Koa = require("koa"); 
+const Router = require("koa-router");
+
 
 const app = new Koa();
+const router = new Router();
+
+router.get("/", async (ctx) => {
+	ctx.body = JSON.stringify(ctx); 
+ 
+});
 
 app.use(async (ctx) => {
-    console.log(ctx);
-
-  ctx.body = JSON.stringify(ctx);
+  ctx.body = JSON.stringify(ctx);  
 });
+
+// allowedMethods 未匹配到的处理
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(3000);
 
---
+++ koa/koa-middleware.js
@@ -0,0 +1,35 @@
+const Koa = require("koa"); 
+const app = new Koa();
+
+
+const middleware = async (ctx,next) => {
+  console.log('middleware start')
+  next()
+  console.log('middleware end')
+}
+const middleware1 = async (ctx,next) => {
+  console.log('middlewar1 start')
+  
+  next()
+
+  console.log('middlewar1e end')
+}
+const middleware2 = async (ctx,next) => {
+  console.log('middleware2 start')
+  next()
+  console.log('middlewar2 end')
+}
+
+
+
+
+app.use(middleware);
+app.use(middleware1);
+app.use(middleware2);
+
+
+
+app.listen(3001);
+
+console.log("Server running at http://localhost:3001/")
+  

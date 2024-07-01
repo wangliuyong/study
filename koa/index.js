2fc02e82f2a51cf5db1dcd214be7521e19d64463
liu.yong.wang.o <liuyong.wang.o@nio.com>
2024-07-01 16:30:39 +0800

    使用koa-body'

--- koa/index.js
+++ koa/index.js
@@ -1,6 +1,7 @@
 const Koa = require('koa');
 const app = new Koa();
 const Router = require('koa-router')
+const koaBody = require('koa-body');
 
 const router = new Router()
 
@@ -8,11 +9,18 @@ router.get('/', ctx => {
   ctx.body = '你好，测不准'
 })
 
+router.post('/post', koaBody(), ctx => {
+    console.log(ctx.request.body);
+    ctx.body = JSON.stringify(ctx.request.body);
+});
+
 
 // 注册路由
 app.use(router.routes())
 // 自动丰富 response 相应头，当未设置响应状态(status)的时候自动设置，在所有路由中间件最后设置(全局，推荐)，也可以设置具体某一个路由（局部），例如：router.get('/index', router.allowedMethods()); 这相当于当访问 /index 时才设置
 app.use(router.allowedMethods())
+
+
 
 
 

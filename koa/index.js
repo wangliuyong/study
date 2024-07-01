45dea5e6bcf5e6ce8d8b422979f80e7a6bf98319
liu.yong.wang.o <liuyong.wang.o@nio.com>
2024-07-01 16:33:46 +0800

    const cors = require('@koa/cors')

--- koa/index.js
+++ koa/index.js
@@ -2,6 +2,7 @@ const Koa = require('koa');
 const app = new Koa();
 const Router = require('koa-router')
 const koaBody = require('koa-body');
+const cors = require('@koa/cors')
 
 const router = new Router()
 
@@ -19,6 +20,11 @@ router.post('/post', koaBody(), ctx => {
 app.use(router.routes())
 // 自动丰富 response 相应头，当未设置响应状态(status)的时候自动设置，在所有路由中间件最后设置(全局，推荐)，也可以设置具体某一个路由（局部），例如：router.get('/index', router.allowedMethods()); 这相当于当访问 /index 时才设置
 app.use(router.allowedMethods())
+
+app.use(cors({
+  origin: '*',    // 前端地址
+  credentials: true
+}));
 
 
 

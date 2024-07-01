b7518765b6ec59f7da4063172a01d812db6dc399
liu.yong.wang.o <liuyong.wang.o@nio.com>
2024-07-01 16:05:42 +0800

    koa-router

--- koa/index.js
+++ koa/index.js
@@ -1,40 +1,42 @@
-const Koa = require("koa"); 
-const Router = require("koa-router");
-
-const KoaBody = require('koa-body'); 
-
-const KoaCors = require("@koa/cors");
-
-
-
-console.log(KoaBody);
-
-
-
+const Koa = require('koa');
 const app = new Koa();
-const router = new Router();
+const Router = require('koa-router')
 
- 
-app.use(KoaCors());   
-app.use(KoaBody());
+const router = new Router()
 
-router.get("/", async (ctx) => {
-	ctx.body = JSON.stringify(ctx); 
-});
-
-app.use(async (ctx) => {
-  ctx.body = JSON.stringify(ctx);  
-});
+router.get('/', ctx => {
+  ctx.body = '你好，测不准'
+})
 
 
-
-
+// 注册路由
 app.use(router.routes())
 // 自动丰富 response 相应头，当未设置响应状态(status)的时候自动设置，在所有路由中间件最后设置(全局，推荐)，也可以设置具体某一个路由（局部），例如：router.get('/index', router.allowedMethods()); 这相当于当访问 /index 时才设置
+app.use(router.allowedMethods())
 
-app.use(router.allowedMethods());
+
+
+// logger
+
+app.use(async (ctx, next) => {
+  await next();
+  const rt = ctx.response.get('X-Response-Time');
+  console.log(`${ctx.method} ${ctx.url} - ${rt}`);
+});
+
+// x-response-time
+
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.set('X-Response-Time', `${ms}ms`);
+});
+
+// response
+
+app.use(async ctx => {
+  ctx.body = 'Hello World';
+});
 
 app.listen(3000);
-
-console.log("Server running at http://localhost:3000/")
-  

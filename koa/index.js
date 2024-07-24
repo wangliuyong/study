1d69252dcdd4cc0f03044fa144665962ad6603d5
liu.yong.wang.o <liuyong.wang.o@nio.com>
2024-07-24 15:48:13 +0800

    save

--- koa/index.js
+++ koa/index.js
@@ -6,46 +6,6 @@ const cors = require("@koa/cors");
 
 const router = new Router();
 
-router.prefix("/api");
-
-// app.use((ctx,next) => {
-//   console.log(1111)
-//   next()
-// })
-
-router.get("/", (ctx) => {
-
-  ctx.body = "你好，测不准";
-});
-
-router.post("/post", koaBody(), (ctx) => {
-    console.log(222)
-  console.log(ctx.request.body);
-  ctx.body = JSON.stringify(ctx.request.body);
-});
-
-
-router.post("/user", koaBody(), (ctx) => {
-      console.log(3333)
-
-  const { name, email } = ctx.request.body;
-  const { admin } = ctx.request.header;
-
-  if (!admin) {
-    ctx.status = 401;
-    ctx.body = "未授权";
-    return
-  }
-  if (name || email) {
-    ctx.body = JSON.stringify(ctx.request.body);
-    return
-  } else {
-    ctx.status = 403;
-    ctx.body = "name or email is required";
-    return
-  }
-});
-
 // 注册路由
 app.use(router.routes());
 // 自动丰富 response 相应头，当未设置响应状态(status)的时候自动设置，在所有路由中间件最后设置(全局，推荐)，也可以设置具体某一个路由（局部），例如：router.get('/index', router.allowedMethods()); 这相当于当访问 /index 时才设置
@@ -57,6 +17,46 @@ app.use(
     credentials: true,
   })
 );
+
+app.use(koaBody());
+
+router.prefix("/api");
+
+// app.use((ctx,next) => {
+//   console.log(1111)
+//   next()
+// })
+
+router.get("/", (ctx) => {
+  ctx.body = "你好，测不准";
+});
+
+router.post("/post", koaBody(), (ctx) => {
+  console.log(222);
+  console.log(ctx.request.body);
+  ctx.body = JSON.stringify(ctx.request.body);
+});
+
+router.post("/user", koaBody(), (ctx) => {
+  console.log(3333);
+
+  const { name, email } = ctx.request.body;
+  const { admin } = ctx.request.header;
+
+  if (!admin) {
+    ctx.status = 401;
+    ctx.body = "未授权";
+    return;
+  }
+  if (name || email) {
+    ctx.body = JSON.stringify(ctx.request.body);
+    return;
+  } else {
+    ctx.status = 403;
+    ctx.body = "name or email is required";
+    return;
+  }
+});
 
 // logger
 

80efa99d93e6654cee08e7adfcb6dff5e80c2690
liuyong.wang.o <liuyonng.wang.o@nio.com>
2024-07-24 16:54:19 +0800

    使用koa-static 加载图片

--- koa/src/index.js
+++ koa/src/index.js
@@ -1,9 +1,18 @@
 const Koa = require("koa");
-const app = new Koa();
+
 const koaBody = require("koa-body");
 const cors = require("@koa/cors");
-
+const helmet = require("koa-helmet");
+const statics = require("koa-static");
+const path = require("path");
 const router = require("./routes");
+
+const app = new Koa();
+
+// 加入安全的请求头配置
+app.use(helmet());
+
+app.use(statics(path.join(__dirname, "../public")));
 
 // 注册路由
 app.use(router());
@@ -18,3 +27,5 @@ app.use(
 app.use(koaBody());
 
 app.listen(3000);
+
+console.log("server is running on port 3000");
